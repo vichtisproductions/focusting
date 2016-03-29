cd95b1bde441a96b9b62c279bd25215ebc1fda82
Renier Oosthuizen <oosthuizen.renier@gmail.com>
2016-03-29 21:02:38 +0200

    Initial Commit

--- Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/Presenter/MainPresenter.java
+++ Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/Presenter/MainPresenter.java
@@ -1,12 +1,9 @@
-package org.coderswithoutborders.deglancer.Presenter;
+package org.coderswithoutborders.deglancer.presenter;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import org.coderswithoutborders.deglancer.model.ActionReceiver;
 import org.coderswithoutborders.deglancer.model.ScreenTime;
 import org.coderswithoutborders.deglancer.view.MainActivity;
-import org.greenrobot.eventbus.EventBus;
 
 import nucleus.presenter.RxPresenter;
 
@@ -15,44 +12,21 @@ import nucleus.presenter.RxPresenter;
  */
 public class MainPresenter extends RxPresenter<MainActivity> {
 
-    private ActionReceiver actionReceiver;
     private ScreenTime screen_time;
-    private EventBus bus = EventBus.getDefault();
 
     @Override
     public void onCreate(Bundle savedState) {
         super.onCreate(savedState);
-        // Initialize  BroadcastReceiver
-        actionReceiver = new ActionReceiver();
-
-        // Register as a subscriber
-        bus.register(this);
     }
 
     @Override
     public void onTakeView(MainActivity view) {
         super.onTakeView(view);
-        actionReceiver.register(view.getApplicationContext());
     }
 
     @Override
     protected void onDestroy() {
-        // Unregister
-        bus.unregister(this);
         super.onDestroy();
-    }
-
-    public void onEvent(String strAction){
-        // Handle different states of the event
-        if (strAction.equals(Intent.ACTION_SCREEN_OFF)) {
-            System.out.println("Screen off " + "LOCKED");
-        } else if (strAction.equals(Intent.ACTION_SCREEN_ON)) {
-            System.out.println("Screen off " + "UNLOCKED");
-        } else if (strAction.equals(Intent.ACTION_POWER_CONNECTED)) {
-            System.out.println("Screen off " + "Connected");
-        } else if (strAction.equals(Intent.ACTION_POWER_DISCONNECTED)) {
-            System.out.println("Screen off " + "Disconnected");
-        }
     }
 }
 
