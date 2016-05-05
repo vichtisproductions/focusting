f635b180fdc2bcb73c3ba2a0651aecd6f964f195
Chris Teli <chris.teli@gmail.com>
2016-05-04 22:42:02 -0700

    Adding a custom toast

--- Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/stagehandlers/Stage1Handler.java
+++ Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/stagehandlers/Stage1Handler.java
@@ -2,13 +2,11 @@ package org.coderswithoutborders.deglancer.stagehandlers;
 
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 import org.coderswithoutborders.deglancer.BuildConfig;
-import org.coderswithoutborders.deglancer.R;
 import org.coderswithoutborders.deglancer.interactor.IDatabaseInteractor;
 import org.coderswithoutborders.deglancer.model.ScreenAction;
-import org.coderswithoutborders.deglancer.utils.TimeUtils;
+import org.coderswithoutborders.deglancer.utils.ToastUtils;
 
 /**
  * Created by Renier on 2016/04/27.
@@ -26,23 +24,21 @@ public class Stage1Handler implements IStageHandler {
     public void handleScreenAction(ScreenAction action) {
         //Only display for dev purposes
         if (BuildConfig.DEBUG) {
-            if (action.getEventType().equals(Intent.ACTION_SCREEN_ON)) {
+            if (action.getEventType().equals(Intent.ACTION_SCREEN_ON)
+                    || action.getEventType().equals(Intent.ACTION_USER_PRESENT)) {
                 long unlockCount = mDatabaseInteractor.getUnlockCountForStageDay(action.getStage(), action.getDay());
                 long totalSOTTime = mDatabaseInteractor.getTotalSOTForStageDay(action.getStage(), action.getDay());
 
-                String lastSleepString = mContext.getString(R.string.toast_last_sleep_label) + " " + TimeUtils.getTimeStringFromMillis(action.getDuration(), true, false, true);
-                String unlockString = mContext.getString(R.string.toast_unlocks_today_label) + " " + unlockCount;
-                String totalSOTTimeString = mContext.getString(R.string.toast_screen_on_today_label) + " " + TimeUtils.getTimeStringFromMillis(totalSOTTime, true, false, true);
+                ToastUtils.showToast(mContext, action.getDuration(), unlockCount, totalSOTTime);
 
-
-                StringBuilder sb = new StringBuilder();
-                sb.append(lastSleepString);
-                sb.append(System.getProperty("line.separator"));
-                sb.append(unlockString);
-                sb.append(System.getProperty("line.separator"));
-                sb.append(totalSOTTimeString);
-
-                Toast.makeText(mContext, sb.toString(), Toast.LENGTH_LONG).show();
+//                StringBuilder sb = new StringBuilder();
+//                sb.append(lastSleepString);
+//                sb.append(System.getProperty("line.separator"));
+//                sb.append(unlockString);
+//                sb.append(System.getProperty("line.separator"));
+//                sb.append(totalSOTTimeString);
+//
+//                Toast.makeText(mContext, sb.toString(), Toast.LENGTH_LONG).show();
 
             }
         }
