c2229302bb304fcbc7aa648c7e987bd58acf82a0
lpalokan <lauri.palokangas@gmail.com>
2016-07-10 07:36:25 +0300

    RES with device ID

--- Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/view/ResearchInformationSheet.java
+++ Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/view/ResearchInformationSheet.java
@@ -1,13 +1,20 @@
 package org.coderswithoutborders.deglancer.view;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 import org.coderswithoutborders.deglancer.MainApplication;
 import org.coderswithoutborders.deglancer.R;
+import org.coderswithoutborders.deglancer.interactor.IUserInteractor;
+
+import java.util.UUID;
 
 /**
  * Created by Lauri Palokangas on 22.6.2016.
@@ -16,11 +23,17 @@ public class ResearchInformationSheet extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.research_information_sheet);
+
+        SharedPreferences prefs = this.getSharedPreferences("UserInteractorSP", Context.MODE_PRIVATE);
+        String deviceIDText = prefs.getString("InstanceId", "");
+
+        ((TextView) findViewById(R.id.tvDeviceIDText)).setText(deviceIDText);
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         Bundle fbAnalyticsBundle = new Bundle();
