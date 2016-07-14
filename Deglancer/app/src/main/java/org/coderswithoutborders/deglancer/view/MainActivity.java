3994e053b3656bbf42d8039078760463a9389e31
lpalokan <lauri.palokangas@gmail.com>
2016-07-14 18:27:44 +0300

    Huawei Protected App fix.

--- Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/view/MainActivity.java
+++ Deglancer/app/src/main/java/org/coderswithoutborders/deglancer/view/MainActivity.java
@@ -1,14 +1,22 @@
 package org.coderswithoutborders.deglancer.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
+import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
 // For RIS button
@@ -32,6 +40,11 @@ import org.coderswithoutborders.deglancer.presenter.IMainActivityPresenter;
 import org.coderswithoutborders.deglancer.R;
 import org.coderswithoutborders.deglancer.pretest.PreTestActivity;
 import org.coderswithoutborders.deglancer.services.TrackerService;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -90,6 +103,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivityView
         }
 
         setStageDependentViewsVisibility();
+        ifHuaweiAlert();
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, null);
@@ -335,5 +349,84 @@ public class MainActivity extends AppCompatActivity implements IMainActivityView
             e.printStackTrace();
         }
     }
+
+
+    // HUAWEI STUFF
+
+    private void ifHuaweiAlert() {
+        final SharedPreferences settings = getSharedPreferences("ProtectedApps", MODE_PRIVATE);
+        final String saveIfSkip = "skipProtectedAppsMessage";
+        boolean skipMessage = settings.getBoolean(saveIfSkip, false);
+        if (!skipMessage) {
+            final SharedPreferences.Editor editor = settings.edit();
+            Intent intent = new Intent();
+            intent.setClassName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity");
+            if (isCallable(intent)) {
+                final AppCompatCheckBox dontShowAgain = new AppCompatCheckBox(this);
+                dontShowAgain.setText("Do not show again");
+                dontShowAgain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        editor.putBoolean(saveIfSkip, isChecked);
+                        editor.apply();
+                    }
+                });
+
+                new AlertDialog.Builder(this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Huawei Protected Apps")
+                        .setMessage(String.format("%s requires to be enabled in 'Protected Apps' to function properly.%n", getString(R.string.app_name)))
+                        .setView(dontShowAgain)
+                        .setPositiveButton("Protected Apps", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                huaweiProtectedApps();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .show();
+            } else {
+                editor.putBoolean(saveIfSkip, true);
+                editor.apply();
+            }
+        }
+    }
+
+    private boolean isCallable(Intent intent) {
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    private void huaweiProtectedApps() {
+        try {
+            String cmd = "am start -n com.huawei.systemmanager/.optimize.process.ProtectActivity";
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                cmd += " --user " + getUserSerial();
+            }
+            Runtime.getRuntime().exec(cmd);
+        } catch (IOException ignored) {
+        }
+    }
+
+    private String getUserSerial() {
+        //noinspection ResourceType
+        Object userManager = getSystemService("user");
+        if (null == userManager) return "";
+
+        try {
+            Method myUserHandleMethod = android.os.Process.class.getMethod("myUserHandle", (Class<?>[]) null);
+            Object myUserHandle = myUserHandleMethod.invoke(android.os.Process.class, (Object[]) null);
+            Method getSerialNumberForUser = userManager.getClass().getMethod("getSerialNumberForUser", myUserHandle.getClass());
+            Long userSerial = (Long) getSerialNumberForUser.invoke(userManager, myUserHandle);
+            if (userSerial != null) {
+                return String.valueOf(userSerial);
+            } else {
+                return "";
+            }
+        } catch (NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException ignored) {
+        }
+        return "";
+    }
+
 
 }
