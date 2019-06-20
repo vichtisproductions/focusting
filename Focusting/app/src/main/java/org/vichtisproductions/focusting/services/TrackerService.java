6d1243236706355ad40dc28584adfbbe4d50f8b3
Taneli Korri <taneli.korri@bitodi.fi>
2019-06-20 17:54:48 +0300

    Create notification channel

--- Focusting/app/src/main/java/org/vichtisproductions/focusting/services/TrackerService.java
+++ Focusting/app/src/main/java/org/vichtisproductions/focusting/services/TrackerService.java
@@ -1,13 +1,17 @@
 package org.vichtisproductions.focusting.services;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
-
-import com.google.firebase.database.DatabaseReference;
 
 import org.vichtisproductions.focusting.MainApplication;
 import org.vichtisproductions.focusting.R;
@@ -56,7 +60,12 @@ public class TrackerService extends Service {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
                 notificationIntent, 0);
 
-        Notification notification = new NotificationCompat.Builder(this)
+        String channelId = "";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelId = createNotificationChannel("tracker_service", "Focusting Tracker Service");
+        }
+
+        Notification notification = new NotificationCompat.Builder(this, channelId)
                 .setContentTitle(getString(R.string.tracker_service_foreground_content_title_text))
                 .setTicker(getString(R.string.tracker_service_foreground_ticker_text))
                 .setContentText(getString(R.string.tracker_service_foreground_content_text))
@@ -68,6 +77,16 @@ public class TrackerService extends Service {
         startForeground(1337, notification);
 
         return START_STICKY;
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private String createNotificationChannel(String channelId, String channelName) {
+        NotificationChannel chan = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        service.createNotificationChannel(chan);
+        return channelId;
     }
 
     @Override
